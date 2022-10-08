f533768dca898fa704ae2f939d4282f1a50ff4c2
HelenaRosalina <115375830+HelenaRosalina@users.noreply.github.com>
2022-10-09 04:38:05 +0700

    Add files via upload

---
+++ PRAK002-2210817220020-Helena Rosalina.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+
+printf("Selamat Pagi, Helena Rosalina \n");
+printf("Selamat Siang, Helena Rosalina \n");
+printf("Selamat Malam, Helena Rosalina \n");
+
+return 0;
+}
---
+++ PRAK004-2210817220020-Helena Rosalina.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+printf("############################## \n");
+printf("#                            # \n");
+printf("#      Helena Rosalina       # \n");
+printf("#       2210817220020        # \n");
+printf("#                            # \n");
+printf("############################## \n");
+
+return 0;
+}
